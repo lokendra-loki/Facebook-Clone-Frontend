e9166c0a880ea495d3d4b7fbbd2d3c0b021bbabd
lokendra <lokendrachaulagain803@gmail.com>
2022-03-27 17:15:11 +0545

    FeedtBar

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Share from '../share/Share'
 import "./feed.scss"
 
 function Feed() {
   return (
-    <div className='feed'>Feed</div>
+    <div className='feed'>
+      <div className="feedWrapper">
+        kmfpokjopfdj
+        <Share/>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/share/Share.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./share.scss"
+
+const Share = () => {
+  return (
+    <div>Share</div>
+  )
+}
+
+export default Share
