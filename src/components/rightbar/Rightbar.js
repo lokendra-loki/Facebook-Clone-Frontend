ff19cdb0ddc28f4d7715b04446b674a2ff5740be
lokendra <lokendrachaulagain803@gmail.com>
2022-03-27 22:02:43 +0545

    FeedBar

--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -3,7 +3,28 @@ import "./rightbar.scss"
 
 function Rightbar() {
   return (
-    <div className='rightBar'>Rightbar</div>
+    <div className='rightBar'>
+      <div className="rightBarWrapper">
+
+        {/* BirthdatContainer------------------------------ */}
+        <div className="birthdayContainer">
+          <img src="/assets/gift.png" alt="" className="birthdayImage" />
+          <span className="birthdayText"><b>Samir</b> and <b>three other friends</b> have a birthday today</span>
+        </div>
+
+        {/* RightBarAd--------------------------------- */}
+        <img src="/assets/ad.png" alt="" className="rightBarAd" />
+        <h4 className="rightBarTitle">Online Friends</h4>
+        <ul className="rightBarFriendList">
+          <div className="rightBarFriend">
+            <div className="rightBarProfileImageContainer">
+              <img src="/assets/person/3.jpeg" alt="" className="rightBarprofileImg" />
+              <span className="rightBarOnline">lok</span>
+            </div>
+          </div>
+        </ul>
+      </div>
+    </div>
   )
 }
 
