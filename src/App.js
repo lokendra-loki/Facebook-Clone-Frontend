f935b9115cc7cc38ccea69706770d1fde42c437b
lokendra <lokendrachaulagain803@gmail.com>
2022-04-08 09:03:43 +0545

    user signup from register page then login from login page then enter into homePage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Home from './pages/home/Home'
-import Register from './components/register/Register'
+import Register from './pages/register/Register'
 import Login from './pages/login/Login'
 import Profile from './pages/profile/Profile'
 import { useContext } from 'react'
---
+++ src/pages/register/Register.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import "./register.scss"
+
+import { useRef } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
+
+
+
+
+function Register() {
+
+    const username = useRef()
+    const email = useRef()
+    const password = useRef()
+    const passwordAgain = useRef()
+
+    const navigate =useNavigate ()
+
+
+
+    const handleClick = async (e) => {
+        e.preventDefault()
+
+        if (passwordAgain.current.value !== password.current.value) {
+            passwordAgain.current.setCustomValidity("password dont match")
+        } else {
+            const user = {
+                username: username.current.value,
+                email: email.current.value,
+                password: password.current.value
+            }
+
+            try {
+                  await axios.post("/auth/register", user)
+                    navigate("/login")
+
+            } catch (err) {
+                console.log(err);
+                
+            }
+
+        }
+    }
+
+
+
+
+
+
+    return (
+        <div className='login'>
+            <div className="loginWrapper">
+
+                {/* Login Left Side--------------------------------*/}
+                <div className="loginLeft">
+                    <h3 className="loginLogo">Facebook</h3>
+                    <span className="loginDescription">Connect with friends and the world around you on Facebook</span>
+                </div>
+
+                {/* LoginRight Side----------------------------------*/}
+                <div className="loginRight">
+                    <form className="loginBox" onSubmit={handleClick}>
+                        <input placeholder="Username" required className="loginInput" ref={username} />
+
+                        <input placeholder="Email" required type="email" minLength="6" className="loginInput" ref={email} />
+
+                        <input placeholder="Create Password" type="password" required className="loginInput" ref={password} />
+
+                        <input placeholder="Confirm Password" type="password" required className="loginInput" ref={passwordAgain} />
+
+                        <button className="loginButton" type="submit" >Sign up</button>
+                        <span className="forgot">Already Have an Account ? LogIn</span>
+                        <button className="registerButton">LogIn</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Register
