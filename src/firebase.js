d8dc5291f42a2715dbdae5a99968bead0ac9a86e
lokendra <lokendrachaulagain803@gmail.com>
2022-06-10 15:42:45 +0545

    env ignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCQneRet7cEewrO1a48PWC50EhAXY_tUjE",
-  authDomain: "facebook-clone-e1567.firebaseapp.com",
-  projectId: "facebook-clone-e1567",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
   storageBucket: "facebook-clone-e1567.appspot.com",
-  messagingSenderId: "457118066393",
-  appId: "1:457118066393:web:3ae0d7a32b67a4f4af00c8"
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-//export 
+  
 export default app;
